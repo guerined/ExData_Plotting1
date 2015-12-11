31b6df4d99f1589d95acf1f99a2a787298e4242f
guerined <david.guerineau@gmail.com>
2015-12-11 21:52:58 +0800

    Good

---
+++ plot2.R
@@ -0,0 +1,7 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
+extract <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+active_power <- as.numeric(extract$Global_active_power)
+date <-strptime(paste(extract$Date, extract$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+png("plot2.png", height=480, width=480)
+plot(date, active_power, type="l",xlab=" ", ylab="Global Active Power (kilowatts)")
+dev.off()
---
+++ plot3.R
@@ -0,0 +1,13 @@
+data <- read.table("household_power_consumption.txt", header=TRUE, sep=";", stringsAsFactors = FALSE, dec=".")
+extract <- data[data$Date %in% c("1/2/2007","2/2/2007"),]
+active_power <- as.numeric(extract$Global_active_power)
+date <-strptime(paste(extract$Date, extract$Time, sep=" "), "%d/%m/%Y %H:%M:%S")
+Sub_metering_1 <-as.numeric(extract$Sub_metering_1)
+Sub_metering_2 <-as.numeric(extract$Sub_metering_2)
+Sub_metering_3 <-as.numeric(extract$Sub_metering_3)
+png("plot3.png", height=480, width=480)
+plot(date, Sub_metering_1, type="l", ylab="Energy Submetering", xlab=" ")
+lines(date, Sub_metering_2, type="l", col="red")
+lines(date, Sub_metering_3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=1, lwd=2.5, col=c("black","red","blue"))
+dev.off()
